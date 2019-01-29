4ff35d763e60236bc5e4d77c3fd0dd8c9a23121a
Nicolas Pellerin <nickypellerin@gmail.com>
2019-01-28 22:21:58 -0500

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,27 +1,34 @@
-import createHistory from 'history/createBrowserHistory';
-import {
-  pageTransitionEvent,
-  pageTransitionTime,
-  pageTransitionExists,
-  componentTransitionTime,
-} from './index.js';
+'use strict';
 
-exports.onClientEntry = (_, { transitionTime }) => {
-  global.window[pageTransitionTime] = transitionTime || 250;
+var _createBrowserHistory = require('history/createBrowserHistory');
+
+var _createBrowserHistory2 = _interopRequireDefault(_createBrowserHistory);
+
+var _index = require('./index.js');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.onClientEntry = function (_, _ref) {
+  var transitionTime = _ref.transitionTime;
+
+  global.window[_index.pageTransitionTime] = transitionTime || 250;
 };
 
-const getUserConfirmation = (pathname, callback) => {
-  const event = new global.window.CustomEvent(pageTransitionEvent, {
-    detail: { pathname },
+var getUserConfirmation = function getUserConfirmation(pathname, callback) {
+  var event = new global.window.CustomEvent(_index.pageTransitionEvent, {
+    detail: { pathname: pathname }
   });
   global.window.dispatchEvent(event);
-  const time =
-    global.window[componentTransitionTime] || global.window[pageTransitionTime];
-  global.window[pageTransitionExists]
-    ? setTimeout(() => callback(true), time)
-    : callback(true);
+  var time = global.window[_index.componentTransitionTime] || global.window[_index.pageTransitionTime];
+  global.window[_index.pageTransitionExists] ? setTimeout(function () {
+    return callback(true);
+  }, time) : callback(true);
 };
 
-const history = createHistory({ getUserConfirmation });
-history.block(location => location.pathname);
-// exports.replaceHistory = () => history;
+var history = (0, _createBrowserHistory2.default)({ getUserConfirmation: getUserConfirmation });
+history.block(function (location) {
+  return location.pathname;
+});
+exports.replaceHistory = function () {
+  return history;
+};
